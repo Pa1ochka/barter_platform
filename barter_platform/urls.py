30ea64cb11cda7e261ee72fa57616f2178145800
Pa1ochka <armen.krut@bk.ru>
2025-05-26 14:56:56 +0500

    настройка документации + гитигнор

--- barter_platform/urls.py
+++ barter_platform/urls.py
@@ -3,13 +3,21 @@ from django.urls import path, include
 from django.views.generic import RedirectView
 from rest_framework.schemas import get_schema_view
 from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 
 schema_view = get_schema_view(
-    title="Barter Platform API",
-    description="API для управления объявлениями и предложениями об обмене",
-    version="1.0.0",
-    permission_classes=[permissions.AllowAny],
+    openapi.Info(
+        title="Barter Platform API",
+        default_version='v1',
+        description="API для платформы обмена вещами",
+        terms_of_service="https://www.example.com/terms/",
+        contact=openapi.Contact(email="contact@example.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
 )
 
 urlpatterns = [
@@ -18,7 +26,7 @@ urlpatterns = [
     path('api/', include('ads.api_urls')),
     path('', include('ads.urls')),
     path('', RedirectView.as_view(url='/ads/', permanent=False)),
-    path('api/schema/', schema_view, name='api-schema'),
-    path('api/docs/', schema_view, name='api-docs'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
 ]
